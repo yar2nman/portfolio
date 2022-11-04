90231c62d818bcb6c148cf268c34d747ae6a0e78
Ahmed <yar2nman@gmail.com>
2022-11-04 23:22:31 +0000

    stack page

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,94 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
+export const SITE_TITLE = 'My Dev Portfolio.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const USER_NAME = 'Ahmed Ibrahim';
+export const ABOUT_ME = ['Hi,', 'I am a mechanical Engineer, Full Stack developer, Chess Player', 'and Grand Father :)))']
+
+export const STACKS = [
+    {
+        name: "Angular",
+        description:
+            "Using Angular starting from Angular js till today on Daily Basis",
+        avatar: "/angular.png",
+        rate: 6,
+    },
+    {
+        name: "React",
+        description:
+            "React with TS is productive and easy to create amazing projects",
+        avatar: "/react.png",
+        rate: 6,
+    },
+    {
+        name: ".Net Core",
+        description:
+            "I like using dot net core for backend projects, It is productive and easy to connect to any database or source like SQL, Postgres, Firebase...",
+        avatar: "/netCore.png",
+        rate: 6,
+    },
+    {
+        name: "Ms SQL",
+        description:
+            "Using SQL for many of my projects, I like the most the ability to query data and combine several tables in a single query with aggregation",
+        avatar: "/sql.png",
+        rate: 7,
+    },
+    {
+        name: "FireBase",
+        description:
+            "Awesome",
+        avatar: "/firebase.png",
+        rate: 5,
+    },
+    {
+        name: "Flask",
+        description:
+            "Flask is easy to use and powerful with all the libraries available for python.",
+        avatar: "/flask.png",
+        rate: 5,
+    },
+    {
+        name: "Node js",
+        description:
+            "",
+        avatar: "/nodeJs.jpg",
+        rate: 4,
+    },
+    {
+        name: "MaterialDesign",
+        description:
+            "",
+        avatar: "/materialDesign.png",
+        rate: 4,
+    },
+    {
+        name: "CSS",
+        description:
+            "",
+        avatar: "/css3.png",
+        rate: 4,
+    },
+    {
+        name: "HTML",
+        description:
+            "",
+        avatar: "/html.png",
+        rate: 4,
+    },
+    {
+        name: "TailWindCSS",
+        description:
+            "",
+        avatar: "/tailwind.png",
+        rate: 4,
+    },
+    {
+        name: "Bootstrap",
+        description:
+            "",
+        avatar: "/bootstrap.jpg",
+        rate: 4,
+    },
+];
