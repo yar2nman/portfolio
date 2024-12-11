065a72d3d38c388af9f698009befffe62035a34a
Ahmed <yar2nman@gmail.com>
2024-12-11 11:33:32 +0300

    flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
   };
 
